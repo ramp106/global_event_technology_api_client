9f3b665b4337020136a5b4aef6d1dc93ef467c26
Ralph von der Heyden <ralph@rvdh.de>
2023-10-24 09:55:26 +0200

    Update GET Swagger URL

--- regenerate_get_client.sh
+++ regenerate_get_client.sh
@@ -1,4 +1,4 @@
 java -jar swagger-codegen-cli.jar generate \
-  -i https://api.get.systems/spec/swaggerv2.json \
+  -i https://api.get.systems/spec/projects/swaggerv2.json \
   -l ruby \
   -c config.json
