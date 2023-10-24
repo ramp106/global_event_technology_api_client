903eab44fd1663c05f7309b502f8d9afc514ac99
Ralph von der Heyden <ralph@rvdh.de>
2023-10-24 10:11:23 +0200

    Remove bundled generator, small changes

--- regenerate_get_client.sh
+++ regenerate_get_client.sh
@@ -1,4 +1,9 @@
-java -jar swagger-codegen-cli.jar generate \
+# Usage: ./regenerate_get_client.sh
+# Description: Regenerates the GET client from the swagger spec
+# Dependencies: swagger-codegen
+# Install with `brew install swagger-codegen`
+
+swagger-codegen generate \
   -i https://api.get.systems/spec/projects/swaggerv2.json \
   -l ruby \
   -c config.json
