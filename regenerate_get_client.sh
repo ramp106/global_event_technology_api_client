8b68d4f7331a2cd9629fabb92205bca4f58b5952
Ralph von der Heyden <ralph@rvdh.de>
2023-10-24 10:52:04 +0200

    Generated code fixes

--- regenerate_get_client.sh
+++ regenerate_get_client.sh
@@ -3,13 +3,25 @@
 # Dependencies: swagger-codegen
 # Install with `brew install swagger-codegen`
 
+
+# STEP 0: Clean up
 rm -rf docs lib spec
+
+
+# STEP 1: Generate the code
 swagger-codegen generate \
   -i https://api.get.systems/spec/projects/swaggerv2.json \
   -l ruby \
   -c config.json
 
-# Fix the generated code
+
+# STEP 2: Fix the generated code
+
+# Contains references to a "Petstore" API (bug in swagger-codegen)
+# https://github.com/swagger-api/swagger-codegen-generators/issues/855
+rm spec/base_object_spec.rb
+
+# Fix illegal constants in this file:
 FILE=lib/global_event_technology_api_client/models/work_time_stamp_type_enum_model.rb
 sed -i '' 's/INFO/_INFO/g' $FILE
 sed -i '' 's/BEGIN/_BEGIN/g' $FILE
